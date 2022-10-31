9937c1a6c2bd0d1cabf0ab2da3e49aa66304e830
Kenny W. Lino <kennywlino@gmail.com>
2022-10-31 15:03:32 -0700

    Setting up Book schema/model.

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+
+const mongoose = require('mongoose');
+
+// *** Mongoose connection
+
+mongoose.connect(process.env.DB_URL);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Mongoose is connected');
+});
+
 const app = express();
 app.use(cors());
 
